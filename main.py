3329e3f9b7373ddc8b36f802d257ecf749cf61a2
ali-kmirzaei <77521376+ali-kmirzaei@users.noreply.github.com>
2022-05-27 10:24:53 +0430

    main file!

---
+++ main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+from pynput.mouse import Listener as Listener_mouse
+from pynput.keyboard import Listener as Listener_keyboard
+from pynput.keyboard import Key
+import logging
+import os
+##################################################################################################
+DURATION = 0.5  # seconds
+FREQ = 440  # Hz
+COUNT = 0
+##################################################################################################
+def logging(action):
+    os.system('play -nq -t alsa synth {} sine {}'.format(DURATION, FREQ))
+    log_file.write('{}\n'.format(action))
+
+def on_move(x, y):
+    global COUNT
+    COUNT += 1
+    if COUNT > 10:
+        logging('mouse moved')
+        COUNT = 0
+
+def on_click(x, y, button, pressed):
+    logging('mouse clicked')
+
+def on_scroll(x, y, dx, dy):
+    logging('mouse scrolled')
+
+def on_press(key):
+    logging('key pressed')
+
+def on_release(key):
+    if key == Key.shift_r:
+        keyboard_listener.stop()
+        mouse_listener.stop()
+        log_file.write('~!EXITED!~\n')
+        log_file.close()
+        exit()
+
+##################################################################################################
+log_file = open('logs.txt', 'w')
+log_file.write('~!Started!~\n')
+
+keyboard_listener = Listener_keyboard(on_press=on_press, on_release=on_release)
+mouse_listener = Listener_mouse(on_move=on_move, on_click=on_click, on_scroll=on_scroll)
+keyboard_listener.start()
+mouse_listener.start()
+keyboard_listener.join()
+mouse_listener.join()
